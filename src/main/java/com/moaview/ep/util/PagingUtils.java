58e1336cc2d8f8a349e24237667cf2dfc39f9303
ytechinfo <ytkiminfo@gmail.com>
2022-04-13 13:44:48 +0900

    메소드명 변경.

--- src/main/java/com/moaview/ep/util/PagingUtils.java
+++ src/main/java/com/moaview/ep/util/PagingUtils.java
@@ -106,12 +106,12 @@ public final  class PagingUtils {
 		return pagingInfo;
 	}
 	
-	public static int getFirstRow(PagingInfo page) {
+	public static int getStartRow(PagingInfo page) {
 		return (page.getCurrPage() - 1) * page.getCountPerPage() + 1;
 	}
 	
 	public static int getEndRow(PagingInfo page) {
-		return getFirstRow(page) + page.getCountPerPage() - 1;
+		return getStartRow(page) + page.getCountPerPage() - 1;
 	}
 
 	private static int getTotalPage(int totalCount, int countPerPage) {
